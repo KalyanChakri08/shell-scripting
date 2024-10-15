54d69787778916c9e82a213f88b1e7ed544581b3
kalyan <kalyanchakri25@gmail.com>
2024-10-15 11:32:23 +0530

    Sample Commit

--- 09-installations.sh
+++ 09-installations.sh
@@ -15,8 +15,17 @@ dnf install mysql -y
 if [ $? -ne 0 ]
 then
     echo "Installation of mysql... FAILURE"
+    exit 1
+else
+    echo "MYSQL installation SUCCESS"
 fi
 
 dnf install git -y
+then
+    echo "Installation of GIT... FAILURE"
+    exit 1
+else
+    echo "GIT installation SUCCESS"
+fi
 
 echo "is script proceeding?"
