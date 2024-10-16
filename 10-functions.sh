566b7b782535937238ba1ead8d8e39b4ff7ecfa1
kalyan <kalyanchakri25@gmail.com>
2024-10-16 15:19:51 +0530

    functions

---
+++ 10-functions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE(){
+    echo "Exit status: $1"
+    echo "What are you doing: $2"
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access"
+    exit 1 #manually exit if error comes
+else
+    echo "You are super user"
+fi
+
+dnf install mysql -y
+
+VALIDATE $? "Installing GIT"
+
+
+dnf install git -y
+
+VALIDATE $? "Installing GIT"
+
+echo "is script proceeding?"
