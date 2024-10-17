abd6282851f45dc6efcc8de4b263cddf30f76418
kalyan <kalyanchakri25@gmail.com>
2024-10-17 12:40:59 +0530

    functions

--- 11-colors.sh
+++ 11-colors.sh
@@ -1,40 +1,46 @@
 #!/bin/bash
 
 USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+TIMESTAMP=$(date +%F-%H-%M-%5)
+
+SCRIPT_NAME=$(echo $e | cut -d "." -f1)
+
+LOGFILE=/tmp/$SCRIPT_NAME-STIMESTAMP.1og
+
 R="\e[31m"
+
 G="\e[32m"
+
 N="\e[0m"
 
 echo "Script started executing at: $TIMESTAMP"
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
+VALIDATE(){ 
+    if [ $1 -ne 0 ] 
     then
-        echo  -e "$2... $R FAILURE $N"
-        exit1
+        echo -e "$2...$R FAILURE $N"
+        exit 1
     else
-        echo -e "$2... $G SUCCESS $N"
+        echo -e "$2...$G SUCCESS $N"
     fi
+
 }
 
-if [ $USERID -ne 0 ]
+if [ $USERID -ne 0 ] 
 then
-    echo "Please run this script with root access"
-    exit 1 #manually exit if error comes
+     echo "Please run this script with root access."
+     exit 1 # manually exit if error comes. 
 else
-    echo "You are super user"
+
+echo "You are super user."
 fi
 
-dnf install mysql -y &>>LOGFILE
+dnf install mysql -y &>>$LOGFILE 
+VALIDATE $? "Installing MySQL"
 
-VALIDATE $? "Installing MYSQL"
+dnf install git -y &>>$LOGFILE 
+VALIDATE $? "Installing Git"
 
-
-dnf install git -y &>>LOGFILE
-
-VALIDATE $? "Installing GIT"
-
-echo "is script proceeding?"
+dnf install dockerr -y &>>$LOGFILE 
+VALIDATE $? "Installing Docker"
